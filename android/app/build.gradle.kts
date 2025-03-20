ade25de0aa4050a49120eb3769a444ceb67d9e62
Choi-Big-Root <bigroot0601@gmail.com>
2025-03-20 13:33:28 +0900

    [20250320] NDK Version 27.0.12077973 으로 변경.

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -11,7 +11,7 @@ plugins {
 android {
     namespace = "com.big_root_market.big_root_market"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
+    ndkVersion = "27.0.12077973"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
