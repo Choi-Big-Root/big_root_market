c1a92dba69b493eadece2b9c0f5b9b47efc0bd19
Choi-Big-Root <bigroot0601@gmail.com>
2025-03-27 14:56:05 +0900

    [20250327] Sign_up Complete
    회원가입 화면 firebaseAuth 연결 및 회원가입 기능 작성.

--- lib/login/sign_up_screen.dart
+++ lib/login/sign_up_screen.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:go_router/go_router.dart';
 
 class SignUpScreen extends StatefulWidget {
   const SignUpScreen({super.key});
@@ -8,11 +11,38 @@ class SignUpScreen extends StatefulWidget {
 }
 
 class _SignUpScreenState extends State<SignUpScreen> {
-  final GlobalKey _formKey = GlobalKey<FormState>();
-  final TextEditingController emailTextEditingController =
-      TextEditingController();
-  final TextEditingController pwdTextEditingController =
-      TextEditingController();
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  String _email = '';
+  String _password = '';
+
+  Future<bool> signUp(String emailAddress, String password) async {
+    try {
+      // auth 이메일, 비밀번호로 사용자 생성.
+      final credential = await FirebaseAuth.instance
+          .createUserWithEmailAndPassword(
+            email: emailAddress,
+            password: password,
+          );
+      // cloud firestore
+      await FirebaseFirestore.instance.collection('users').add({
+        'uid': credential.user?.uid ?? '',
+        'email': credential.user?.email ?? '',
+      });
+      return true;
+    } on FirebaseAuthException catch (e) {
+      if (e.code == 'invalid-password') {
+        debugPrint('등록하실 비밀번호는 6자리 이상의 문자여야 합니다.');
+      } else if (e.code == 'invalid-email') {
+        debugPrint('이메일이 잘못되었습니다.');
+      } else if (e.code == 'email-already-exists') {
+        debugPrint('이미 존재하는 이메일 입니다.');
+      }
+
+      return false;
+    } catch (e) {
+      return false;
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -21,69 +51,90 @@ class _SignUpScreenState extends State<SignUpScreen> {
       body: SafeArea(
         child: Padding(
           padding: const EdgeInsets.all(48.0),
-          child: Column(
-            children: [
-              const Text(
-                'BigRoot 마트\n가입을 환영합니다.',
-                style: TextStyle(fontSize: 36, fontWeight: FontWeight.bold),
-              ),
-              const SizedBox(height: 24),
-              Form(
-                key: _formKey,
-                child: Column(
-                  children: [
-                    TextFormField(
-                      controller: emailTextEditingController,
-                      decoration: const InputDecoration(
-                        border: OutlineInputBorder(),
-                        labelText: '이메일',
-                      ),
-                      keyboardType: TextInputType.emailAddress,
-                      validator: (value) {
-                        if (value == null || value.isEmpty) {
-                          return '이메일을 입력하세요.';
-                        }
-                        if (!value.contains('@')) {
-                          return '이메일 형식이 아닙니다.';
-                        }
-                        return null;
-                      },
-                    ),
-                    const SizedBox(height: 24),
-                    TextFormField(
-                      controller: pwdTextEditingController,
-                      decoration: const InputDecoration(
-                        border: OutlineInputBorder(),
-                        labelText: '비밀번호',
-                      ),
-                      obscureText: true,
-                      keyboardType: TextInputType.visiblePassword,
-                      validator: (value) {
-                        if (value == null || value.isEmpty) {
-                          return '비밀번호를 입력해주세요.';
-                        }
-                        return null;
-                      },
-                    ),
-                  ],
+          child: SingleChildScrollView(
+            child: Column(
+              children: [
+                const Text(
+                  'BigRoot 마트\n가입을 환영합니다.',
+                  style: TextStyle(fontSize: 36, fontWeight: FontWeight.bold),
                 ),
-              ),
-              const SizedBox(height: 24),
-              MaterialButton(
-                minWidth: double.infinity,
-                height: 48,
-                color: Colors.red,
-                onPressed: () {},
-                child: const Text(
-                  '회원가입',
-                  style: TextStyle(
-                    fontSize: 16,
-                    color: Colors.white,
-                    fontWeight: FontWeight.bold,
+                const SizedBox(height: 24),
+                Form(
+                  key: _formKey,
+                  child: Column(
+                    children: [
+                      TextFormField(
+                        decoration: const InputDecoration(
+                          border: OutlineInputBorder(),
+                          labelText: '이메일',
+                        ),
+                        keyboardType: TextInputType.emailAddress,
+                        validator: (value) {
+                          if (value == null || value.isEmpty) {
+                            return '이메일을 입력하세요.';
+                          }
+                          if (!value.contains('@')) {
+                            return '이메일 형식이 아닙니다.';
+                          }
+                          return null;
+                        },
+                        onSaved: (value) {
+                          _email = value!.trim();
+                        },
+                      ),
+                      const SizedBox(height: 24),
+                      TextFormField(
+                        decoration: const InputDecoration(
+                          border: OutlineInputBorder(),
+                          labelText: '비밀번호',
+                        ),
+                        obscureText: true,
+                        keyboardType: TextInputType.visiblePassword,
+                        validator: (value) {
+                          if (value == null || value.isEmpty) {
+                            return '비밀번호를 입력해주세요.';
+                          }
+                          return null;
+                        },
+                        onSaved: (value) => _password = value!.trim(),
+                      ),
+                    ],
                   ),
                 ),
-              ),
-            ],
+                const SizedBox(height: 24),
+                MaterialButton(
+                  onPressed: () async {
+                    if (_formKey.currentState!.validate()) {
+                      _formKey.currentState!.save();
+
+                      final result = await signUp(_email, _password);
+
+                      if (!context.mounted) return;
+
+                      if (result) {
+                        ScaffoldMessenger.of(context).showSnackBar(
+                          const SnackBar(content: Text('회원가입 성공 하셨습니다.')),
+                        );
+                        //go() 현재화면을 경로에 맞는 화면으로 변경하는 개념.
+                        //go_router 패키지를 사용한다면 pop을 실행후 push를 해야 동일한기능을 할것으로 생각.
+                        context.go('/login');
+                      }
+                    }
+                  },
+                  minWidth: double.infinity,
+                  height: 48,
+                  color: Colors.red,
+                  child: const Text(
+                    '회원가입',
+                    style: TextStyle(
+                      fontSize: 16,
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       ),
