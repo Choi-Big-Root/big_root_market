a452edc498aa0259bed74ba8117cd1cfb8810d6a
Choi-Big-Root <bigroot0601@gmail.com>
2025-03-27 10:46:45 +0900

    [20250327] Go_Router Setting
    go_router 패키지를 적용.

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,12 @@
+import 'package:big_root_market/home/cart_screen.dart';
+import 'package:big_root_market/home/home_screen.dart';
+import 'package:big_root_market/home/product_add_screen.dart';
+import 'package:big_root_market/home/product_detail_screen.dart';
 import 'package:big_root_market/login/login_screen.dart';
+import 'package:big_root_market/login/sign_up_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:go_router/go_router.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 
 void main() async {
@@ -12,21 +18,52 @@ void main() async {
     anonKey: dotenv.env['SUPABASE_ANON_KEY']!,
   );
 
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  MyApp({super.key});
+
+  final GoRouter _router = GoRouter(
+    initialLocation: '/login',
+    routes: [
+      GoRoute(
+        path: '/',
+        builder: (context, state) => const HomeScreen(),
+        routes: [
+          GoRoute(
+            path: 'cart/:uid',
+            builder:
+                (context, state) =>
+                    CartScreen(uid: state.pathParameters['uid'] ?? ''),
+          ),
+          GoRoute(
+            path: 'product',
+            builder: (context, state) => const ProductDetailScreen(),
+          ),
+          GoRoute(
+            path: 'product/add',
+            builder: (context, state) => const ProductAddScreen(),
+          ),
+        ],
+      ),
+      GoRoute(path: '/login', builder: (context, state) => const LoginScreen()),
+      GoRoute(
+        path: '/sign_up',
+        builder: (context, state) => const SignUpScreen(),
+      ),
+    ],
+  );
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return MaterialApp.router(
       debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
+      title: 'BigRoot마트',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
-      home: const LoginScreen(),
+      routerConfig: _router,
     );
   }
 }
