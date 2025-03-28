7e68b49a1e509cd1252ce4d1fb3ec4f6739a1f24
Choi-Big-Root <bigroot0601@gmail.com>
2025-03-28 17:15:40 +0900

    [20250328] Camera pakage add
    카메라 패키지 추가 및 iOS 권한 설정 + product_add_screen 불필요한 import 제거

--- lib/home/product_add_screen.dart
+++ lib/home/product_add_screen.dart
@@ -3,7 +3,6 @@ import 'dart:typed_data';
 import 'package:big_root_market/model/category.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_image_compress/flutter_image_compress.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 
--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,7 @@ import 'package:big_root_market/home/product_add_screen.dart';
 import 'package:big_root_market/home/product_detail_screen.dart';
 import 'package:big_root_market/login/login_screen.dart';
 import 'package:big_root_market/login/sign_up_screen.dart';
+import 'package:camera/camera.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
@@ -14,9 +15,13 @@ import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:go_router/go_router.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 
+late List<CameraDescription> _cameras;
+
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await dotenv.load();
+
+  _cameras = await availableCameras();
 
   //firebase Storage 가 무료버전이 없어 대체가능한 supabase를 사용.
   await Supabase.initialize(
@@ -28,7 +33,7 @@ void main() async {
   if (kDebugMode) {
     try {
       //구글 로그인은 localemulator에서 동작하지 않아 임시로 주석처리.
-      //await FirebaseAuth.instance.useAuthEmulator('localhost', 9099);
+      await FirebaseAuth.instance.useAuthEmulator('localhost', 9099);
       FirebaseFirestore.instance.useFirestoreEmulator('localhost', 8080);
       //final supabase = Supabase.instance.client;
     } catch (e) {
